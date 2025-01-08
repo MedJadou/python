b2ab17f9cee297b56a7ce952d470f1f28a30fd51
medjadou <srayi.rajae@gmail.com>
2025-01-08 18:31:17 +0100

    function

--- print.py
+++ print.py
@@ -1,9 +1,4 @@
-str = "codingal"
-for char in str:
-  print(char)
-
-
-
-# range(start_val end_val+1, step_size)
-for i in range(0, 11, 2):
-    print(i)
+def intro(name):
+ print("Hello , Good morning! I am", name)
+name = input("Enter your name")
+print("Hello , Good morning! I am", name)
